3f0aa3d266cb7497417bdf5c858e08433e7e6eb8
JustDezz <amichalenko@mail.ru>
2021-05-13 13:41:51 +0300

    Добавил кубы для стекла, капсулы для пластика, подправил ItemBehaviour.cs (теперь, если объект оказался выше области обзора камеры, он упадёт, как положено, а ещё немного уменьшил допустимую область для объектов). Также позиция возврата теперь корректно сохраняется при подъёме объекта
    Signed-off-by: JustDezz <amichalenko@mail.ru>

--- Assets/Scripts/Instantiiating.cs
+++ Assets/Scripts/Instantiiating.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class Instantiiating : MonoBehaviour
 {
     [SerializeField] GameObject[] objects;
+    public float dispersion = 1f;
     void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
     {
         for (int i = 0; i < objects.Length; i++)
         {
-            Instantiate(objects[i], transform.position, Quaternion.identity, transform);
+            Vector3 tempX = Vector3.right * Random.Range(-dispersion, dispersion) * 3;
+            Vector3 tempZ = Vector3.forward * Random.Range(-dispersion, dispersion) * 5;
+            Vector3 newPosition = transform.position + tempX + tempZ;
+            GameObject garbage = Instantiate(objects[i], newPosition, Quaternion.identity, transform);
+            garbage.GetComponent<Rigidbody>().velocity = (tempX + tempZ + Vector3.down * 2);
+            yield return new WaitForSeconds(0.1f);
         }
     }
 }
--- Assets/Scripts/ItemBehaviour.cs
+++ Assets/Scripts/ItemBehaviour.cs
@@ -10,27 +10,27 @@ public class ItemBehaviour : MonoBehaviour
 	private void Start()
 	{
 		rigidbody = GetComponent<Rigidbody>();
-		returnPosition = transform.position;
+		//returnPosition = transform.position;
 	}
 	private void FixedUpdate()
 	{
 		Vector3 screenPoint = Camera.main.WorldToViewportPoint(gameObject.transform.position);
-		if (screenPoint.x < 0 || screenPoint.x > 1)
+		if (screenPoint.x < 0.05 || screenPoint.x > 0.95)
 		{
 			rigidbody.velocity = new Vector3(rigidbody.velocity.x * (-1), rigidbody.velocity.y, rigidbody.velocity.z);
 		}
-		if (screenPoint.y < 0 || screenPoint.y > 1)
-		{
-			rigidbody.velocity = new Vector3(rigidbody.velocity.x, rigidbody.velocity.y * (-1), rigidbody.velocity.z);
-		}
-		if (screenPoint.z < 0 || screenPoint.z > 1)
+		//if (screenPoint.y < 0 || screenPoint.y > 1)
+		//{
+		//	rigidbody.velocity = new Vector3(rigidbody.velocity.x, rigidbody.velocity.y * (-1), rigidbody.velocity.z);
+		//}
+		if (screenPoint.z < 0.4 || screenPoint.z > 0.9)
 		{
 			rigidbody.velocity = new Vector3(rigidbody.velocity.x, rigidbody.velocity.y, rigidbody.velocity.z * (-1));
 		}
 		if (transform.position.y < -20)
         {
 			transform.position = new Vector3(transform.position.x, 5, transform.position.y);
-        }
+		}
 	}
 	public void SavePosition()
 	{
